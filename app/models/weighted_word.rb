a370792f10428d130ad73295d503bd3d386bd008
chaeokay <chae@bignerdranch.com>
2016-02-01 20:51:09 -0500

    Changes weighted word from PORO to AR model
    [#6]

--- app/models/weighted_word.rb
+++ app/models/weighted_word.rb
@@ -1,12 +1,10 @@
 class WeightedWord < ActiveRecord::Base
-  include Comparable
-
   belongs_to :word_search, inverse_of: :weighted_words
 
   validates :word, presence: true
+  validates :word_search, presence: true
   validates :frequency, presence: true
 
-  def <=>(other_word)
-    other_word.frequency <=> frequency
-  end
+  scope :sorted_by_frequency, -> { order(:frequency).reverse }
+  scope :top_ten, -> { sorted_by_frequency.first(10) }
 end
