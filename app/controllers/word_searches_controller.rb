b3a207af7f0194041886ec76ec7da945845ed3bc
chaeokay <chae@bignerdranch.com>
2016-02-02 20:18:38 -0500

    Refactors controller, fixes up errors messages to be all encompassing
    * word_search has errors on the object still, but whatevs. errors in
      objects could be collected, but it's just not worth it.

--- app/controllers/word_searches_controller.rb
+++ app/controllers/word_searches_controller.rb
@@ -8,7 +8,7 @@ class WordSearchesController < ApplicationController
     @word_search = WordSearch.where(word_search_params).first_or_initialize
     if @word_search.persisted?
       render :show
-    elsif valid_url? && @word_search.weigh_url_words
+    elsif valid_url? && make_weighted_words(@word_search)
       render :show
     else
       flash.now[:notice] = I18n.t('word_search.errors.unparsable')
@@ -27,6 +27,15 @@ class WordSearchesController < ApplicationController
 
   def word_search_params
     params.require(:word_search).permit(:url)
+  end
+
+  def make_weighted_words(word_search)
+    WeightedWordSearchMaker.new(word_search: word_search, words_map: url_words)
+      .make_weighted_words
+  end
+
+  def url_words
+    UrlWords.new(url: word_search_params[:url]).words_map
   end
 
   def valid_url?
--- app/models/word_search.rb
+++ app/models/word_search.rb
@@ -16,29 +16,4 @@ class WordSearch < ActiveRecord::Base
   def top_weighted_frequency
     heaviest.frequency
   end
-
-  def weigh_url_words
-    WeightedWordSearchMaker.new(words: url_words, word_search: self)
-      .make_weighted_words
-  end
-
-  private
-
-  def invalid_url_error
-    self.errors[:url] << I18n.t('word_search.errors.url')
-    false
-  end
-
-  def parsable_url?
-    parsed_url = URI.parse(url)
-    parsed_url.kind_of?(URI::HTTP) || parsed_url.kind_of?(URI::HTTPS)
-  end
-
-  def heaviest_weighted_word
-    weighted_words.sorted_by_frequency.first
-  end
-
-  def url_words
-    UrlWords.new(url: url).all
-  end
 end
